6415274af56998c3174d99d5e014f3d1469217fc
arfrie22 <43021241+arfrie22@users.noreply.github.com>
2025-10-04 12:21:50 -0400

    fix backup.sh

--- bitwarden/backup.sh
+++ bitwarden/backup.sh
@@ -1,10 +1,13 @@
 bw config server $BW_SERVER
-if [[ $(bw status) == *"unauthenticated"* ]]; then
-  bw login $BW_EMAIL --passwordenv BW_PASSWORD > tmp
+
+status=$(bw status | jq -r '.status')
+echo "$status"
+if [ "$status" = "unauthenticated" ]; then
+  bw login $BW_EMAIL --passwordenv BW_PASSWORD > tmp_file
 else
-  bw unlock --passwordenv BW_PASSWORD > tmp
+  bw unlock --passwordenv BW_PASSWORD > tmp_file
 fi
 
-export BW_SESSION=$(grep "export BW_SESSION=" tmp | sed 's/\$ export BW_SESSION=//')
+export BW_SESSION=$(grep "export BW_SESSION=" tmp_file | sed 's/\$ export BW_SESSION=//')
 
 bw export --organizationid $BW_ORG --format json --output /backup/
